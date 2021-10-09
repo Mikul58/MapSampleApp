3cf3f1cb5702f987912ef67220d1a0a15fdc607e
Mikul58 <3tcmikulski@gmail.com>
2021-10-09 12:44:45 +0200

    Dodaj pliki projektów.

---
+++ MapSampleApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace MapSampleApp.ViewModels
+{
+    class MainPageViewModel
+    {
+        public MainPageViewModel()
+        {
+            var status = Task.Run(async ()=> await Permissions.RequestAsync<Permissions.LocationWhenInUse>());
+        }
+    }
+}
